94989ca7c336b7eb1044d3261bcb4014f3715247
Diane Tuyizere <dtuyizere@smith.edu>
2025-12-19 00:05:21 -0500

    Worked on the tests and the bugs they are exposing plus the java docs

--- ShopTest.java
+++ ShopTest.java
@@ -1,10 +1,20 @@
 import org.junit.Test;
+<<<<<<< HEAD
 import static org.junit.Assert.*;
 import java.util.ArrayList;
+=======
+/*
+* Testing for the bugs in the resaleshop and computer.
+ */
+>>>>>>> b46d398 (Worked on the tests and the bugs they are exposing plus the java docs)
 
 
 public class ShopTest {
+    /*Test Computer buy
+    * @throws Exception when comp is not in inventory but bought
+     */
 
+<<<<<<< HEAD
     // add your tests here 
     @Test
     /**
@@ -92,3 +102,101 @@ public class ShopTest {
     }
 
 
+=======
+    @Test
+    public void testBuy_AddNewComputer()throws Exception{
+        Computer NewComputer=new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        ResaleShop myShop=new ResaleShop();
+        myShop.buy(NewComputer);
+        assertTrue(myShop.inventory.contains(NewComputer));
+
+
+    }
+    /*Tests buy and the throw of the exception if the comp is already in the inventory
+    *@ throw Exception if comp is already in the inventory
+     */
+    @Test
+    public void testBuy_throwExceptionIfCompExixts(){
+        Computer NewComputer=new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        ResaleShop myShop=new ResaleShop();
+        myShop.buy(NewComputer);
+        assertThrows(Exception.class,()->myShop.buy(NewComputer));
+    
+}
+/*
+*Tests the Operating System
+ */
+@Test
+    public void testSetOS(){
+        Computer NewComputer=new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        assertEquals("HighSierra", NewComputer.operatingSystem);
+    }
+    /*
+*Tests the Computer Set price
+ */
+@Test
+    public void testconstructorSetPrice(){
+        Computer NewComputer=new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        assertEquals(1000, NewComputer.price);
+    }
+      /*
+*Tests the Computer Set price
+ */
+@Test
+    public void testComputerComputerConstructor_setMemory(){
+        Computer NewComputer=new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        assertEquals(16, NewComputer.memory);
+    } 
+    /*Tests sell is made
+    *@ throw Exception if comp is still in the inventory
+     */
+@Test
+    public void testsell_removeNewComputer()throws Exception{
+        Computer NewComputer=new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        ResaleShop myShop=new ResaleShop();
+        myShop.sell(NewComputer);
+        assertFalse(myShop.inventory.contains(NewComputer));
+    }
+    /*Tests sell and the throw of the exception if the comp is not removed in the inventory
+    *@ throw Exception if comp is is not removed
+     */
+    @Test
+    public void testSell_throwExceptionIfCompNotInInventory(){
+        Computer NewComputer=new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        ResaleShop myShop=new ResaleShop();
+        myShop.sell(NewComputer);
+        assertThrows(Exception.class,()->myShop.sell(NewComputer));
+    
+}
+/*
+* Testing the account on selling a computer yet the inventory is empty at all
+ */
+@Test
+    public void testInventoryNotEmpty()throws Exception{
+        Computer NewComputer=new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        ResaleShop myShop=new ResaleShop();
+        myShop.sell(NewComputer);
+        assertFalse(myShop.isEmpty());
+    }
+    /*
+* Testing for testPrintInventoryIndexOutOfBounds based on the loop used
+ */
+@Test
+    public void testPrintInventoryIndexOutOfBounds(){
+        Computer NewComputer=new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        ResaleShop myShop=new ResaleShop();
+        assertThrows(IndexOutOfBoundsException.class,() -> myShop.printInventory());
+    }
+       /*
+* Testing for the OS updates
+ */
+   @Test
+    public void testRefurbishUpdatesOS(){
+        Computer NewComputer=new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        ResaleShop myShop=new ResaleShop();
+        myShop.refurbish(NewComputer, "NewOS");
+        assertEquals( "NewOs",NewComputer.operatingSystem );
+    }
+
+     
+>>>>>>> b46d398 (Worked on the tests and the bugs they are exposing plus the java docs)
--- bugLog.md
+++ bugLog.md
@@ -1,39 +1,44 @@
 ## Bug 1
+<<<<<<< HEAD
 Brief description:  The constuctutor is suppossed to return the instance of the ResaleShop class which is theinventory and it is not returning anything.
 Failed unit test: public ArrayList< Computer>test_ShopReturn()
+=======
+Brief description:  Buy method does not throw the exception when computer is not addeded but bought. Basically it does not check if the computer was sccessfully bought. 
+Failed unit test: testBuy_AddNewComputer()
+>>>>>>> b46d398 (Worked on the tests and the bugs they are exposing plus the java docs)
 
 ## Bug 2
-Brief description:  
-Failed unit test: 
+Brief description: Buy method does not throw the exception if computer alredy exists in the inventory 
+Failed unit test:testBuy_throwExceptionIfCompExixts() 
 
 ## Bug 3
-Brief description:  
-Failed unit test: 
+Brief description: Sell methods in resaleshop does not throw theexception if computer is not in the inmventory 
+Failed unit test: testSell_throwExceptionIfCompNotInInventory()
 
 ## Bug 4
-Brief description:  
-Failed unit test: 
+Brief description: Set Os in computer does not set it as required 
+Failed unit test: testSetOS()
 
 ## Bug 5
-Brief description:  
-Failed unit test: 
+Brief description: The computor constructor does not update the price respective to the constructor parameters
+Failed unit test:testconstructorSetPrice() 
 
 ## Bug 6
-Brief description:  
-Failed unit test: 
+Brief description: Print inventory loop does not print all the objects in the inventory 
+Failed unit test: testPrintInventoryIndexOutOfBounds()
 
 ## Bug 7
-Brief description:  
-Failed unit test: 
+Brief description: Computer constructor does not initilize the the value given to the memory 
+Failed unit test:testComputerComputerConstructor_setMemory() 
 
 ## Bug 8
-Brief description:  
-Failed unit test: 
+Brief description:  sell method does not throw exception when computer is not removed from the inventory
+Failed unit test: testsell_removeNewComputer()
 
 ## Bug 9
-Brief description:  
-Failed unit test: 
+Brief description: The sell computer needs not only to check if the inventory is not empty at all 
+Failed unit test:testInventoryNotEmpty() 
 
 ## Bug 10
-Brief description:  
-Failed unit test: 
+Brief description: Refurbish doesn't update the OS as required
+Failed unit test:testRefurbishUpdatesOS() 
