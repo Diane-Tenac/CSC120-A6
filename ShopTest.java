915a3c08fe74ad4218650e110b3ee3767c1a9987
Diane Tuyizere <dtuyizere@smith.edu>
2025-12-19 00:54:33 -0500

    Shop test updates

--- ShopTest.java
+++ ShopTest.java
@@ -1,12 +1,8 @@
-import org.junit.Test;
-<<<<<<< HEAD
 import static org.junit.Assert.*;
-import java.util.ArrayList;
-=======
+import org.junit.Test;
 /*
 * Testing for the bugs in the resaleshop and computer.
  */
->>>>>>> b46d398 (Worked on the tests and the bugs they are exposing plus the java docs)
 
 
 public class ShopTest {
@@ -14,95 +10,6 @@ public class ShopTest {
     * @throws Exception when comp is not in inventory but bought
      */
 
-<<<<<<< HEAD
-    // add your tests here 
-    @Test
-    /**
-     * Constructor
-     *
-     * @return an instance of ResaleShop
-     */
-    
-
-    
-
-    @Test
-    /* This computer should have all the key attributes registered in */
-    public void test_computerDiscription() {
-        ResaleShop ResaleShop = new ResaleShop();
-        Computer newComputer = new Computer("2020 MacBookAir Retina", "Intel", 256, 16, "Sonoma", 2019, 1000);
-        assertEquals("2020 MacBookAir Retina", newComputer.description);
-    }
-
-    @Test
-    public void test_computerProcessorType() {
-        ResaleShop ResaleShop = new ResaleShop();
-        Computer newComputer = new Computer("2020 MacBookAir Retina", "Intel", 256, 16, "Sonoma", 2019, 1000);
-        assertEquals("Intel", newComputer.processorType);
-    }
-
-    @Test
-    public void test_computerhardDriveCapacity() {
-        ResaleShop ResaleShop = new ResaleShop();
-        Computer newComputer = new Computer("2020 MacBookAir Retina", "Intel", 256, 16, "Sonoma", 2019, 1000);
-        assertEquals(256, newComputer.hardDriveCapacity);
-    }
-
-    @Test
-    public void test_computerMemory(){
-        ResaleShop ResaleShop = new ResaleShop();
-        Computer newComputer = new Computer("2020 MacBookAir Retina", "Intel", 256, 16, "Sonoma", 2019, 1000);
-        assertEquals(16, newComputer.memory);
-    }
-
-    @Test
-    public void test_computerOperatingSystem(){
-        ResaleShop ResaleShop = new ResaleShop();
-        Computer newComputer = new Computer("2020 MacBookAir Retina", "Intel", 256, 16, "Sonoma", 2019, 1000);
-        assertEquals("Sonoma", newComputer.operatingSystem);
-    }
-    @Test
-    public void test_exceptionTrow(){
-        ResaleShop ResaleShop = new ResaleShop();
-        Computer newComputer = new Computer("2020 MacBookAir Retina", "Intel", 256, 16, "Sonoma", 2019, 1000);
-        
-        try {
-            ResaleShop.inventory.add(newComputer);
-            if (ResaleShop.inventory.size() > 1 ){
-                throw new Exception("Computer already in inventory");
-            }
-
-        } catch (Exception e) {
-            assertEquals("Computer already in inventory", e.getMessage());
-            }
-    }
-
-    public void test_computerSell() {
-        ;
-        
-        }
-
-
-    }
-
-    public void test_inventoryPrint() {
-
-    }
-
-    public void test_refurbished() {
-
-    }
-
-    public void test_newOs() {
-
-    }
-
-    public static void main(String[] args) {
-
-    }
-
-
-=======
     @Test
     public void testBuy_AddNewComputer()throws Exception{
         Computer NewComputer=new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
@@ -199,4 +106,3 @@ public class ShopTest {
     }
 
      
->>>>>>> b46d398 (Worked on the tests and the bugs they are exposing plus the java docs)
